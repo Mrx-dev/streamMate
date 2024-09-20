a8fad39ab817f75745b7f96e9151d21be5b5d3ff
Mrx-dev <mrxdevreply@gmail.com>
2024-09-20 07:35:10 +0300

    Adding dynamic file log

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,9 +1,16 @@
 import { createLogger, format, transports } from 'winston';
 import path from 'path';
+import { format as formatDate } from 'date-fns';
 
 const logFormat = format.printf(({ level, message, timestamp }) => {
   return `${timestamp} [${level}]: ${message}`;
 });
+
+const getLogFileName = (): string => {
+  const now = new Date();
+  const formattedDate = formatDate(now, 'yyyy-MM-dd HH-mm-ss'); // Avoid invalid characters like ':' in filenames
+  return `${formattedDate}-streamMate.log`;
+};
 
 export const logger = createLogger({
   level: 'info',
@@ -11,7 +18,7 @@ export const logger = createLogger({
   transports: [
     new transports.Console(),
     new transports.File({
-      filename: path.join('./logs', 'streamMate.log'),
+      filename: path.join('./logs', getLogFileName()),
     }),
   ],
 });
